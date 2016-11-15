6c05b02db3f776e56dc52f83fd8c2cf3c5ce7541
John Hatton <hattonjohn@gmail.com>
2016-11-15 09:54:46 -0700

    1st attempt at wiring up password reset

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var SimpleSendGridAdapter = require('parse-server-sendgrid-adapter');
+
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
@@ -15,11 +17,20 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  masterKey: process.env.MASTER_KEY || '',
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+
+  //password reset
+  emailAdapter: SimpleSendGridAdapter({
+    apiKey: process.env.SENDGRID_API_KEY,
+    fromAddress: 'reset@bloomlibrary.org',
+  }),
+  publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // apparently used by password reset emailer
+  verifyUserEmails:true,
+  appName:'BloomLibrary.org'
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
