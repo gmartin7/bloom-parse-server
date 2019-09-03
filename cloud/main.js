bbbe16b85007e737444bc6396e9d68368d3439c9
Jeffrey Su <jeffrey_su@sil.org>
2019-09-03 09:38:02 -0700

    Add harvestLog field for Bloom Harvester

--- cloud/main.js
+++ cloud/main.js
@@ -612,7 +612,7 @@ Parse.Cloud.define("setupTables", function(request, response) {
                 {name: "harvestState", type:"String"},
                 {name: "harvesterId", type:"String"},
                 {name: "harvestStartedAt", type:"Date"},
-                {name: "warnings", type:"Array"},
+                {name: "harvestLog", type:"Array"},
                 // End fields required by Harvester
                 {name: "internetLimits", type:"Object"}
             ]
